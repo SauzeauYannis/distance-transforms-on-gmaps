f172705f865ab8580213589cd2aab20fdc201129
LordCatello <carmine_carratu@libero.it>
2021-10-04 23:31:58 +0200

    Added some tests to evaluate performances in multiple situations.

--- performance/evaluate_performance_pyramidal_dt.py
+++ performance/evaluate_performance_pyramidal_dt.py
@@ -1,6 +1,7 @@
+import typing
 from unittest import TestCase
 
-import numpy as np
+import random
 import math
 
 from distance_transform.wave_propagation import wave_propagation_dt_image
@@ -11,27 +12,44 @@ from distance_transform.dt_utils import *
 
 class EvaluatePerformancePyramidalDt(TestCase):
     def setUp(self) -> None:
-        pass
+        random.seed(42)
 
-    def test_mae_one_background_pixel(self):
-        for i in range(2, 7):
-            image_size = 2**i
-            stride = 2**(i-1)
-            image = np.ones((image_size, image_size), dtype=int)
+    def generate_random_image(self, image_size: int, background_pixel_probability: float) -> np.array:
+        image = np.ones((image_size, image_size), dtype=int)
 
-            # set central pixel to 0
-            image[math.floor(image_size / 2)][math.floor(image_size / 2)] = 0
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                random_float = random.random()
+                if random_float < background_pixel_probability:
+                    image[i][j] = 0
 
-            dt_image = wave_propagation_dt_image(image)
+        return image
+
+    def evaluate_mae_with_multiple_strides(self, image, strides: typing.List[int]) -> None:
+        plot_binary_image(image)
+        dt_image = wave_propagation_dt_image(image)
+        plot_dt_image(dt_image)
+        for stride in strides:
             approximate_dt_image = pyramidal_dt_binary_image(image, stride)
 
             mae = mae_image(dt_image, approximate_dt_image)
-            print(f"mae with image_size {image_size} is {mae} and stride {stride}")
+            print(f"mae with image_size {image.shape[0]} is {mae} and stride {stride}")
 
             # Show images
-            plot_dt_image(dt_image)
             plot_dt_image(approximate_dt_image)
-            """
-            """
 
+    def test_mae_one_background_pixel(self):
+        image_size = 128
+        image = np.ones((image_size, image_size), dtype=int)
+        # set central pixel to 0
+        image[math.floor(image.shape[0] / 2)][math.floor(image.shape[1] / 2)] = 0
+
+        # evaluate the performance on the same image with different strides (from 2 to 64)
+        self.evaluate_mae_with_multiple_strides(image, [2, 4, 8, 16, 64])
+        self.assertTrue(True)
+
+    def test_mae_random_images(self):
+        image = self.generate_random_image(128, 0.01)
+        # evaluate the performance on the same image with different strides (from 2 to 64)
+        self.evaluate_mae_with_multiple_strides(image, [2, 4, 8, 16, 64])
         self.assertTrue(True)
